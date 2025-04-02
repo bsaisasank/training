53325644361669929dda6bf5fb11e03d3b2fba32
kavya2430 <162956077+kavya2430@users.noreply.github.com>
2025-04-02 12:23:09 +0530

    buttonchnages

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,5 +1,24 @@
+import React from 'react';
 import styles from './button.module.scss';
 
-export const Button = () => {
-  return <div className={styles['test']}>askdnaskldns</div>;
+interface ButtonProps {
+  text?: string;
+  icon?: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info';
+  type?: 'outline' | 'default';
+  fullWidth?: boolean; // New property
+  onClick?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ text, icon, variant = 'primary', type = 'default', fullWidth = false, onClick }) => {
+  const buttonClass = `${styles.button} ${type === 'outline' ? styles.outline : ''} ${styles[variant]} ${
+    fullWidth ? styles.fullWidth : ''
+  }`;
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {icon && <span className={styles.icon}>{icon}</span>}
+      {text && <span className={styles.text}>{text}</span>}
+    </button>
+  );
 };
